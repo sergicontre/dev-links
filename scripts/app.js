aebef262706d8118cbeb1c690bee76ce63072152
Tomasz Dziuda <dziudek@gmail.com>
2015-06-02 18:50:16 +0200

    Another fixes

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,8 @@
 	var source   = $("#issue-template").html();
 	var template = Handlebars.compile(source);
 	
-	$.ajax({
-		"url": "issues/1.json"
-	}).done(function(data) {
+	$.getJSON("issues/1.json", function(data) {
 		var issue_content = template(data);
-	
 		$('.issue').html(issue_content);
 	});
 })(jQuery);
