0eab0b6c6788199c8201ab2cbc6a1109b5a8e6e3
Tomasz Dziuda <dziudek@gmail.com>
2015-06-03 00:54:30 +0200

    Added second issue and improved script to load always the last issue

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,12 @@
 	var source   = $("#issue-template").html();
 	var template = Handlebars.compile(source);
 	
-	$.getJSON("issues/1.json", function(data) {
-		var issue_content = template(data);
-		$('.issue').html(issue_content);
+	$.getJSON("issues/issues.json", function(data) {
+		var last = data.last;
+		
+		$.getJSON("issues/"+last+".json", function(data) {	
+			var issue_content = template(data);
+			$('.issue').html(issue_content);
+		});
 	});
 })(jQuery);
