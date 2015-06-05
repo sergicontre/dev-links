bd9f616cd7f9c1e78720cab737f3ac734e183be2
Tomasz Dziuda <dziudek@gmail.com>
2015-06-06 00:49:44 +0200

    Initial navigation between issues.

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,49 @@
 (function($) {
 	var source   = $("#issue-template").html();
 	var template = Handlebars.compile(source);
+	var last = 0;
 	
 	$.getJSON("issues/issues.json", function(data) {
-		var last = data.last;
+		last = data.last;
 		
 		$.getJSON("issues/"+last+".json", function(data) {	
+			if(parseInt(last) > parseInt(data.number)) {
+				data.next = true;
+				data.nextNumber = data.number + 1;
+			}
+			
+			if(parseInt(data.number) > 1) {
+				data.prev = true;
+				data.prevNumber = data.number - 1;
+			}
+			
 			var issue_content = template(data);
 			$('.issue').html(issue_content);
 		});
+	});
+	
+	$('.issue').click(function(e) {
+		var toLoad = false;
+		
+		if($(e.target).hasClass('next') || $(e.target).hasClass('prev')) {
+			toLoad = $(e.target).attr('href').replace('#', '');
+		}
+		
+		if(toLoad) {
+			$.getJSON("issues/"+last+".json", function(data) {	
+				if(parseInt(last) > parseInt(data.number)) {
+					data.next = true;
+					data.nextNumber = data.number + 1;
+				}
+				
+				if(parseInt(data.number) > 1) {
+					data.prev = true;
+					data.prevNumber = data.number - 1;
+				}
+				
+				var issue_content = template(data);
+				$('.issue').html(issue_content);
+			});
+		}
 	});
 })(jQuery);
