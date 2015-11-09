c8e64fb8be499c221e4204497002f03e3203ff0c
Tomasz Dziuda <dziudek@gmail.com>
2015-11-09 13:26:06 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 (function($) {
 	var source   = $("#issue-template").html();
 	var template = Handlebars.compile(source);
-	var last = 29;
+	var last = 30;
 	
 	if(
 		window.location.hash !== '' && 
