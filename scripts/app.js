38136b9665b58052756dcc6648201613da2e7379
sergio.contreras <sergio.contreras.martin@bbva.com>
2016-03-25 16:57:33 +0100

    feat(*): change name of repo and user of github

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,44 @@
 (function($) {
 	var source   = $("#issue-template").html();
 	var template = Handlebars.compile(source);
+	var last = 49;
 	
-	$.ajax({
-		"url": "issues/1.json"
-	}).done(function(data) {
-		var issue_content = template(data);
+	if(
+		window.location.hash !== '' && 
+		parseInt(window.location.hash.replace('#', '')) > 0 && 
+		window.location.hash.replace('#', '') <= last
+	) {
+		loadContent(window.location.hash.replace('#', ''));
+	} else {
+		loadContent(last);
+	}
 	
-		$('.issue').html(issue_content);
+	$('.issue').click(function(e) {
+		var toLoad = false;
+		
+		if($(e.target).hasClass('next') || $(e.target).hasClass('prev')) {
+			toLoad = $(e.target).attr('href').replace('#', '');
+		}
+		
+		if(toLoad) {
+			loadContent(toLoad);
+		}
 	});
+	
+	function loadContent(issue) {
+		$.getJSON("issues/"+issue+".json?token=" + new Date().getTime(), function(data) {	
+			if(parseInt(last) > parseInt(data.number)) {
+				data.next = true;
+				data.nextNumber = data.number * 1 + 1;
+			}
+			
+			if(parseInt(data.number) > 1) {
+				data.prev = true;
+				data.prevNumber = data.number * 1 - 1;
+			}
+			
+			var issue_content = template(data);
+			$('.issue').html(issue_content);
+		});
+	}
 })(jQuery);
